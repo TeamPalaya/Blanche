8d2bc2a449db44b6a40686cc85b3db60c6e6174b
SpringDL <DanL4@live.ca>
2014-10-26 04:03:41 -0400

    Movement WIP

---
+++ Assets/Resources/Scripts/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	private GameObject universe;
+	private Universe universeSS;
+
+	// Use this for initialization
+	void Start () {
+		universe = GameObject.Find("Universe");
+		universeSS = universe.GetComponent<Universe>();
+
+		//Make the bullet fly forwards @start
+		//universeSS.Move(this.gameObject, 8, 70f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//universeSS.Move(this.gameObject, 8, 10f);
+	}
+}
