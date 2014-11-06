db630b7802a9811fd969aee7d474317cf49abc17
Jan Kevin Munar <jankevinmunar@gmail.com>
2014-11-05 21:28:50 -0500

    Test Commit
    Testing new commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public Transform destination;
+
+	// Update is called once per frame
+	void Update () {
+		transform.GetComponent<NavMeshAgent>().destination = destination.transform.position;
+	}
+}
