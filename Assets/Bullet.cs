e7eb5328f39bec54dc24931472b05f49da60cdfa
AJBucur <alexandrebucur@gmail.com>
2014-11-07 11:21:30 -0500

    Boss Movement Commit 1
    Basis for Boss movement and attack. Focus on Boss movement in relation
    to Blance

---
+++ Assets/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour
+{
+    //Explosion Effect
+    public GameObject Explosion;
+
+    public float Speed = 0.2f;
+    public float LifeTime = 5.0f;
+    public int damage = 0;
+
+    void Start()
+    {
+        Destroy(gameObject, LifeTime);
+    }
+
+    void Update()
+    {
+        transform.position += 
+			transform.forward * Speed * Time.deltaTime;       
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        ContactPoint contact = collision.contacts[0];
+        Instantiate(Explosion, contact.point, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
