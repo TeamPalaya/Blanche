91a3a4b0e42c16c54a30508467821118853461ec
ryans3 <ryan.s3@hotmail.com>
2014-11-09 15:56:11 -0500

    Minion2-Flight #54 #55 #56 #62

---
+++ BlancheV2/Assets/Resources/Scripts/Navigation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Navigation : MonoBehaviour {
+
+	public bool lookAtPlayer = false;
+
+	// Use this for initialization
+	void Start () {
+		gameObject.GetComponent<NavMeshAgent>().destination = GameObject.Find("Goal").transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (lookAtPlayer){
+			transform.LookAt(GameObject.Find("Player").transform);
+		}
+	}
+
+	void OnTriggerEnter (Collider other){
+		if (other.gameObject.name == "Player"){
+			lookAtPlayer = true;
+		}
+	}
+}
