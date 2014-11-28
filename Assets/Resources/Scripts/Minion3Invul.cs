9fda6a7914035f158e1eb1e6b53d44a4f0d2db9b
SpringDL <DanL4@live.ca>
2014-11-27 22:37:06 -0500

    #130  #131  #129  Minion3 blocks dmg while guarding. Also cooldown on guard

---
+++ Assets/Resources/Scripts/Minion3Invul.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Minion3Invul : MonoBehaviour {
+
+	public Collider blancheMeleeAttack;
+	public Collider blanchRangedAttack;
+
+	public bool isGuarding = false;
+	public bool isInvul = false;
+
+	public GameObject minion3;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollision (Collision other){
+		if (isGuarding){
+			if (other.collider == blancheMeleeAttack || other.collider == blanchRangedAttack && !isInvul){
+				isInvul = true;
+				StartCoroutine("InvulTimer");
+			}
+			StartCoroutine("GuardTimer");
+		}
+	}
+
+	IEnumerator InvulTimer (){
+		yield return new WaitForSeconds(2f);
+		isInvul = false;
+	}
+
+	IEnumerator GuardTimer (){
+		yield return new WaitForSeconds(4f);
+		isGuarding = false;
+	}
+}
