9bccd173e57108fc6612fc0f7013b8d8758ccca2
Jan Kevin Munar <jankevinmunar@gmail.com>
2014-11-28 14:30:59 -0500

    Added Minion 3 and Player Damage calculations and hp loss #124 #125 #126
    Also includes a secondary minion 3

---
+++ Assets/Resources/Minion3/Scripts/Bullet.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	public GameObject Explosion;
+
+	public float speed = 0.05f;
+	public float LifeTime = 5.0f;
+	
+	public int damage = 10;
+
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject, LifeTime);
+	}
+
+	void Update()
+	{
+		transform.position += transform.forward * speed * Time.deltaTime;
+	}
+	
+	// Destroy bullet
+	void OnCollisionEnter(Collision collision)
+	{
+		if(collision.gameObject.tag == "Player")
+		{
+			print("Player is hit by bullet! Player health is now " + collision.gameObject.GetComponent<Blanche>().health);
+			collision.gameObject.GetComponent<Blanche>().health -= damage;
+		}
+		if (collision.gameObject.tag != "Enemy") 
+		{
+			Destroy (gameObject);
+		}
+
+	}
+}
---
+++ Assets/Resources/Scripts/FSM.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FSM : MonoBehaviour {
+
+	protected Transform playerposition;
+
+	protected Vector3 destPos;
+	protected float shootRate;
+	protected float elapsedTime;
+
+	protected Vector3 destinationpos;
+	
+	protected GameObject[] pointList;
+
+	public Transform bulletSpawnPoint{ get; set; }
+
+	protected virtual void Initialize(){ }
+	protected virtual void FSMUpdate() { }
+	protected virtual void FSMFixedUpdate() { }
+
+	// Use this for initialization
+	void Start () {
+		Initialize ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		FSMUpdate ();
+	}
+
+	void FiexedUpdate()
+	{
+		FSMFixedUpdate ();
+	}
+}
