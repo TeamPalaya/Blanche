a3bcddda979cc03b7d70435e19a7dfb1adb839fc
AJBucur <alexandrebucur@gmail.com>
2014-12-07 14:20:25 -0500

    User menu Commit 1
    Finished all tasks relating to the user story "User can view menu."

---
+++ Assets/ButtonScripts.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScripts : MonoBehaviour {
+
+	public GameObject optionsMenu, panel;
+
+	// Use this for initialization
+	void Start () {
+		//AudioSource.PlayClipAtPoint(new AudioClip().name(""), transform.position);
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+
+	public void StartGame(){
+		print ("load level");
+		Application.LoadLevel("LoadedLevel");
+	}
+
+	public void ExitGame(){
+		print ("sdfkjsdfkjsfd");
+		Application.Quit();
+	}
+
+	public void ViewOptions(){
+		optionsMenu.SetActive(true);
+
+	}
+
+	public void backButton(){
+		optionsMenu.SetActive(false);
+
+	}
+}
